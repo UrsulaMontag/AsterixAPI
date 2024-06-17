f8f4057ceea71c44b064808913c86994a4b233ae
ugla <montagu666@gmail.com>
2024-06-17 16:01:49 +0200

    Load characters from mongo db

--- src/main/java/u/m/asterixapi/controller/AsterixController.java
+++ src/main/java/u/m/asterixapi/controller/AsterixController.java
@@ -1,29 +1,23 @@
 package u.m.asterixapi.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import u.m.asterixapi.models.Character;
+import u.m.asterixapi.models.CharacterRepo;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/asterix/characters")
+@RequiredArgsConstructor
 public class AsterixController {
+
+    private final CharacterRepo characters;
 
     @GetMapping
     public List<Character> getCharacters() {
-        return List.of(
-                new Character("1", "Asterix", 35, "Krieger"),
-                new Character("2", "Obelix", 35, "Lieferant"),
-                new Character("3", "Miraculix", 60, "Druide"),
-                new Character("4", "Majestix", 60, "Häuptling"),
-                new Character("5", "Troubadix", 25, "Barden"),
-                new Character("6", "Gutemine", 35, "Häuptlingsfrau"),
-                new Character("7", "Idefix", 5, "Hund"),
-                new Character("8", "Geriatrix", 70, "Rentner"),
-                new Character("9", "Automatix", 35, "Schmied"),
-                new Character("10", "Grockelix", 35, "Fischer")
-        );
+        return characters.findAll();
     }
 }
--- src/main/java/u/m/asterixapi/models/CharacterRepo.java
+++ src/main/java/u/m/asterixapi/models/CharacterRepo.java
@@ -1,6 +1,9 @@
 package u.m.asterixapi.models;
 
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CharacterRepo extends MongoRepository<Character, String> {
 }
